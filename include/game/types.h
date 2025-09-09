75108b6d18abe449efbba100fed8693499bbfa37
Thowen102 <76935401+Thowen102@users.noreply.github.com>
2025-09-08 17:33:48 -0700

    Create types.h

---
+++ include/game/types.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "bn_fixed_point.h"
+#include "bn_point.h"
+#include "bn_optional.h"
+#include "bn_span.h"
+
+
+enum class Ability { None, Dash, Fire, Spark, Stone };
+
+
+enum class EnemyKind { Walker, Flyer, Turret };
